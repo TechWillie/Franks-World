bbe2640477c4589cd253c629e7408cb2a97ee257
TechWillie <techwillie5@gmail.com>
2025-04-21 17:25:36 -0400

    working on landing page. Just ideas nothing official

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// import mypic from '../../../backend/media/pictures/me-standing.jpg';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='frontPage'>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        
+          {/* <img src="../public/media/pictures/me-standing.jpg" className="logo" alt="Vite logo" /> */}
+      
+        <a href="www.rapid-exposure.com" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Franks world</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
