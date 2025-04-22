18d69183b81a8a95bb7bdc24c27002dac1c908bc
TechWillie <techwillie5@gmail.com>
2025-04-22 02:38:42 -0400

    Improve,emts to landing page. Not finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,33 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 // import mypic from '../../../backend/media/pictures/me-standing.jpg';
-
+import { quotes } from './justSaying'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [index, setIndex] = useState(0)
+
+  const handleClick = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length)
+    setIndex(randomIndex)
+  }
 
   return (
-    <div className='frontPage'>
-      <div>
-        
-          {/* <img src="../public/media/pictures/me-standing.jpg" className="logo" alt="Vite logo" /> */}
+    <div className='body'>
       
-        <a href="www.rapid-exposure.com" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Franks world</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1 className='rotate'>Franks World</h1>
+      <div>
+      <button className="card" onClick={handleClick}>
+          Just sayin... {quotes[index]}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+       
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </div>
   )
 }
---
+++ frontend/src/justSaying.jsx
@@ -0,0 +1,8 @@
+export const quotes = [
+    <h1>Get out your feelings..<br></br> Strictly business..!</h1>,
+    <h1>A failure to plan<br></br>is a plan to fail</h1>,
+    <h1>If you do all the cryin..<br></br> what the babies gone do..?</h1>,
+    <h1>Struggle should equal progression..<br></br> If you're doin it right..?</h1>,
+    <h1>Don't surround yourself with ediots..<br></br> Just because it's easy...</h1>
+
+]
