f5c35b5854699cacf1037b6bb5a12d8136409432
TechWillie <techwillie5@gmail.com>
2025-04-26 01:19:56 -0400

    User login complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,22 +1,55 @@
 const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
+// const csrf = require('csurf');
+// const csrfProtection = csrf({ cookie: true });
+
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+// const user = require('../../db/models/user');
 
 
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-    try {
-      const users = await User.findAll();
-      res.json({ users });
-    } catch (err) {
-      next(err);
+
+// Login User
+router.post(
+  '/',
+  async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.unscoped().findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential
+        }
+      }
+    });
+
+    if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Franks World Login failed';
+      err.errors = { credential: 'The provided credentials were invalid.' };
+      return next(err);
     }
-  });
-  
+
+    const safeUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    };
+
+    await setTokenCookie(res, safeUser);
+
+    return res.json({
+      user: safeUser
+    });
+  }
+);
+
 
 module.exports = router;
