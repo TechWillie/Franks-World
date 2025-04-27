b6062c1f02dbcf1e9518dbb678e9829529898c9d
TechWillie <techwillie5@gmail.com>
2025-04-27 15:29:02 -0400

    table revision: putback id in seeders: 0.1.5

--- backend/db/seeders/20250424040424-seed-places.js
+++ backend/db/seeders/20250424040424-seed-places.js
@@ -3,10 +3,10 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('Places', [
-      { placeId: 1, name: 'Central Park', location: 'NYC', createdAt: new Date(), updatedAt: new Date(), description: 'A large public park in the heart of Manhattan.' },
-      { placeId: 2, name: 'Golden Gate Park', location: 'San Francisco', createdAt: new Date(), updatedAt: new Date(), description: 'A large urban park in San Francisco.' },
-      { placeId: 3, name: 'Millennium Park', location: 'Chicago', createdAt: new Date(), updatedAt: new Date(), description: 'A large public park in the heart of Chicago.' },
-      { placeId: 4, name: 'Balboa Park', location: 'San Diego', createdAt: new Date(), updatedAt: new Date(), description: 'A large urban park in San Diego.' }
+      { name: 'Central Park', location: 'NYC', createdAt: new Date(), updatedAt: new Date(), description: 'A large public park in the heart of Manhattan.' },
+      { name: 'Golden Gate Park', location: 'San Francisco', createdAt: new Date(), updatedAt: new Date(), description: 'A large urban park in San Francisco.' },
+      { name: 'Millennium Park', location: 'Chicago', createdAt: new Date(), updatedAt: new Date(), description: 'A large public park in the heart of Chicago.' },
+      { name: 'Balboa Park', location: 'San Diego', createdAt: new Date(), updatedAt: new Date(), description: 'A large urban park in San Diego.' }
     ]);
   },
   async down(queryInterface) {
