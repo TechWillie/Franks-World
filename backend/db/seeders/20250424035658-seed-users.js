3f73af8745920125f06274809ca15758664d6133
TechWillie <techwillie5@gmail.com>
2025-04-27 14:46:30 -0400

    table revision 0.0.6

--- backend/db/seeders/20250424035658-seed-users.js
+++ backend/db/seeders/20250424035658-seed-users.js
@@ -14,7 +14,7 @@ module.exports = {
 
     await queryInterface.bulkInsert('Users', [
       {
-        id: 1,
+        
         username: 'alice',
         firstName: 'Alice',
         lastName: 'Smith',
@@ -24,7 +24,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: 2,
+        
         username: 'bobby',
         firstName: 'Bobbyish',
         lastName: 'Brownie',
@@ -34,7 +34,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: 3,
+        
         username: 'carol',
         firstName: 'Diddy',
         lastName: 'Didit',
@@ -44,7 +44,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: 4,
+        
         username: 'dave',
         firstName: 'Dave',
         lastName: 'Busters',
